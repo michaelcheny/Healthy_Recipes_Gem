351ccb04c9bc79006440050429270a0d5acadb9e
Michael Chen <michaelchengaming@gmail.com>
2019-06-13 22:42:36 -0700

    got recipe list looping correctly, simplified my loops

--- lib/healthy_recipes/cli.rb
+++ lib/healthy_recipes/cli.rb
@@ -42,28 +42,49 @@ class CLI
     self.choose_category
   end
 
+
   def choose_category
-
     puts "\n\nPlease choose a valid category number: (or 'exit' to quit)\n"
+    user_selected_index = gets.strip
 
-    user_selected_index = nil
-
-    until user_selected_index == "exit" || "back"
-      user_selected_index = gets.strip
-
-      if user_selected_index.to_i.between?(1, Recipes.get_category_names.length)
-        category = Recipes.get_category_names[user_selected_index.to_i - 1]
-        self.choose_recipes_from_category(category)
-      elsif user_selected_index == "back"
-        self.go_back
-      elsif user_selected_index == "exit"
-        CLI.
-      else
-        puts "Please choose a valid category number: (or 'exit' to quit)"
-      end
-
+    if user_selected_index.to_i.between?(1, Recipes.get_category_names.length)
+      category = Recipes.get_category_names[user_selected_index.to_i - 1]
+      self.choose_recipes_from_category(category)
+    elsif user_selected_index == "exit"
+      self.farewell
+      exit
+    else
+      self.reject_input
+      sleep(2)
+      self.list_categories
     end
+
   end
+
+  ###backuip
+  # def choose_category
+
+  #   puts "\n\nPlease choose a valid category number: (or 'exit' to quit)\n"
+
+  #   user_selected_index = nil
+
+  #   until user_selected_index == "exit" || user_selected_index == "back"
+  #     user_selected_index = gets.strip
+
+  #     if user_selected_index.to_i.between?(1, Recipes.get_category_names.length)
+  #       category = Recipes.get_category_names[user_selected_index.to_i - 1]
+  #       self.choose_recipes_from_category(category)
+  #     elsif user_selected_index == "back"
+  #       self.go_back
+  #     elsif user_selected_index == "exit"
+  #       CLI.exit
+  #     else
+  #       puts "Please choose a valid category number: (or 'exit' to quit)"
+  #     end
+
+  #   end
+  # end
+
 
 
   def choose_recipes_from_category(selected_category)
@@ -72,18 +93,23 @@ class CLI
     recipes.each.with_index(1){|recipe_object, index| puts "#{index}. #{recipe_object.name}"}
     # binding.pry
     puts "\n\nPlease choose a recipe number: (or 'exit' to exit)\n"
-    recipe_index = nil
-    until recipe_index.to_i.between?(1, recipes.length)
+    # recipe_index = nil
+    # until recipe_index.to_i.between?(1, recipes.length)
     recipe_index = gets.strip
-      if recipe_index.to_i.between?(1, recipes.length)
-        selected_recipe = recipes[recipe_index.to_i - 1]
-        self.get_recipe_info(selected_recipe) 
-      elsif recipe_index == "back"
-        self.go_back
-      else 
-        puts "Please choose a recipe number: (or 'exit' to exit)"
-      end
+    if recipe_index.to_i.between?(1, recipes.length)
+      selected_recipe = recipes[recipe_index.to_i - 1]
+      self.get_recipe_info(selected_recipe) 
+    elsif recipe_index == "back"
+      self.go_back
+    elsif recipe_index == "exit"
+      self.farewell
+      exit
+    else 
+      self.reject_input
+      sleep(2)
+      self.choose_recipes_from_category(selected_category)
     end
+    # end
   end
 
 
@@ -112,14 +138,14 @@ class CLI
 
     self.seperator
     puts "\n\n"
-    # self.ask_user_what_do_now
+    self.ask_user_what_do_now
   end
 
   def ask_user_what_do_now
     puts "Enter 'back' to go back or 'exit' to exit':"
     user_input = gets.strip.downcase
     if user_input == "back"
-      self.go_back
+      return
     elsif user_input == "exit"
       self.exit
     end
@@ -130,9 +156,8 @@ class CLI
     return
   end
 
-  def exit
-    puts "\nThanks for trying this app out. Goodbye\n"
-    return
+  def farewell
+    puts "\nThanks for trying this app out. Goodbye\n\n"
   end
 
  
@@ -144,7 +169,7 @@ class CLI
 
 
   def reject_input
-    puts "Invalid input, please try again."
+    puts "\nInvalid input, try again."
   end
 
   def seperator
